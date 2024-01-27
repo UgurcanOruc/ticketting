7517b8e39c8535e9c7d416206be15ff4cc06de5c
UgurcanOruc <ugurcan-oruc@hotmail.com>
2024-01-27 18:10:11 +0300

    * some npms installed to help providing hashed password and jwt tokens, and also cookies. * password hashing and compare functions created. * bad request error handling custom error class created. *

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,37 +1,53 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequetError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid.'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20})
-        .withMessage('Password must be between 4-20 characters.')
-], async (req: Request, res : Response) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid."),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4-20 characters."),
+  ],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+      throw new RequestValidationError(errors.array());
     }
-    
+
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-        console.log('Email is use.');
-        return res.send({});
+      throw new BadRequetError("Email in use.");
     }
 
-    const user = User.build({ email, password});
+    const user = User.build({ email, password });
     await user.save();
 
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asdas"
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
     res.status(201).send(user);
-});
+  }
+);
 
 export { router as signupRouter };
