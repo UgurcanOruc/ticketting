0fd9e4886696fd936edf95e9c490a8f138144c96
UgurcanOruc <ugurcan-oruc@hotmail.com>
2024-02-04 21:24:31 +0300

    * custom hook created to use in different routes. * signup route completed. * index.js can now get current user info with any rendering type, server side or client side

---
+++ client/hooks/use-request.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      const repsonse = await axios[method](url, body);
+      setErrors(null);
+      
+      if (onSuccess) {
+        onSuccess(repsonse.data);
+      }
+      
+      return repsonse.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Oooops....</h4>
+          <ul className="my-0">
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from 'next/router';
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+        email, password
+    },
+    onSuccess: () => Router.push('/')
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await doRequest();
+
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          className="form-control"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
