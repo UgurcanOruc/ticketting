80634e7a425928c1272557851e11a2cb76853f54
UgurcanOruc <ugurcan-oruc@hotmail.com>
2024-02-07 21:34:01 +0300

    *Tickets service created. * create ticket route and its test file completed. * Docker deployment and service files created. * Skaffold modified for tickets.

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,50 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "ugur";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany();
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  // Create the JWT.
+  // Build session object.
+  // Turn that session into JSON.
+  // Take JSON and encode it as base64.
+  // return a string thats the cookiie with the encoded data.
+
+  const payload = {
+    id: '123ugur',
+    email: 'test@test.com'
+  }
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  return [`session=${base64}`];
+};
