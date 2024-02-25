ad1fcb90cc1082e51923ffa350075a5ab96a7a4a
UgurcanOruc <ugurcan-oruc@hotmail.com>
2024-02-25 16:06:45 +0300

    nats test service finished. Now it will be added to real project

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -6,16 +7,16 @@ const stan = nats.connect('tickets', 'abc', {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS.');
-
-    const data = JSON.stringify({
-        id: '123',
-        title: 'concert',
-        price: 20
-    });
-
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    });
+    const publisher = new TicketCreatedPublisher(stan);
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20
+        })
+    } catch (err) {
+        console.log(err);
+    }
 }); 
